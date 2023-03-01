4fb2fec72eaecc6c60a03a6a4c8fbd7cf6629150
Sergey <seslik627@gmail.com>
2023-03-01 11:21:50 +0200

    eslint

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const numberOfFilms =+prompt("Сколько фильмов вы уже посмотрели?", "");
 
 const personalMovieDB = {
-    cout: numberOfFilms,
-    movie:{},
-    actors:{},
-    genres:{},
-    privat:false
+	cout: numberOfFilms,
+	movie:{},
+	actors:{},
+	genres:{},
+	privat:false
 };
 
 const a = prompt("Один из последних просмотренных фильмов", ""),
-      b = prompt("На сколько оцените его",""),
-      c = prompt("Один из последних просмотренных фильмов", ""),
-      d = prompt("На сколько оцените его","");
+	b = prompt("На сколько оцените его",""),
+	c = prompt("Один из последних просмотренных фильмов", ""),
+	d = prompt("На сколько оцените его","");
       
 personalMovieDB.movie[a] = b ;    
 personalMovieDB.movie[c] = d ;     
