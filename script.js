72aa366dcda21427070cfd4f72e8271a9bfdbdf7
Sergey <seslik627@gmail.com>
2023-03-01 13:02:08 +0200

    20lesson

--- script.js
+++ script.js
@@ -16,3 +16,37 @@ const a = prompt("Один из последних просмотренных ф
 personalMovieDB.movie[a] = b ;    
 personalMovieDB.movie[c] = d ;     
 console.log(personalMovieDB);
+
+"use strict";
+if (4 == 0) {
+	console.log("ok");
+} else {
+	console.log("error");
+}
+
+
+if(num < 49) {
+	console.log("Error");
+} else if (num > 100) {
+	console.log("Много");
+} else {
+	console.log("ok");
+}
+
+(num == 50) ? console.log("ok!") : console.log ("Error");
+
+const num = "50";
+switch (num) {
+case "49":
+	console.log("Неверно");
+	break;
+case "100":
+	console.log("Неверно");
+	break;
+case "50":
+	console.log("В точку");
+	break;
+default:
+	console.log("Не в этот раз");
+	break;
+}
